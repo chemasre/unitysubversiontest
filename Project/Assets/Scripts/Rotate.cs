cbba162deaf8ee9ebd4c140f841964c25e34bb11
chemasre <72490746+chemasre@users.noreply.github.com>
2020-12-11 02:32:28 +0100

    + Unity project

---
+++ Project/Assets/Scripts/Rotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed = 100.0f;
+
+    float rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotation = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation += speed * Time.deltaTime;
+
+        transform.rotation = Quaternion.Euler(rotation, 0, 0);
+    }
+}
